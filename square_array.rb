a0eaef5b98b614cc7fa3c13dc21ecda4ce141ed6
Kathy Hudson <khitx@icloud.com>
2018-01-12 19:39:02 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,5 +1,9 @@
+#def square_array(array)
+#  newarray = []
+#  array.each {|item| newarray.push(item*item)}
+#  newarray
+#end
+
 def square_array(array)
-  newarray = []
-  array.each {|item| newarray.push(item*item)}
-  newarray
+  array.collect {|item| item*item}
 end
